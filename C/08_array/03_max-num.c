600a6ef0209cc147ea9511f030da232db6b5dc3b
Priyesh Singh <priyeshwas11@gmail.com>
2024-12-08 13:10:28 +0530

    made program in array

---
+++ C/08_array/03_max-num.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int n;
+    printf("enter the the number of elements of array : ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter the %d element of the array : ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int i, max = INT_MAX;
+    for (i = 0; i < n; i++)
+    {
+        if (max > arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    printf("the minimum number of array is %d", max);
+
+    return 0;
+}
