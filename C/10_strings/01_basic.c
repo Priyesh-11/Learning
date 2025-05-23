0192e3f58bd7d6a3b754390e46a3bfe3f04fe589
Priyesh <Priyeshwas11@gmail.com>
2025-05-23 21:23:19 +0530

    add basic string handling example in C

---
+++ C/10_strings/01_basic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    // Declare a string
+    // char arr[]; // array of characters
+    // char arr[] = {'P','r','i','y','e','s','h','\0'}; // string initialization
+
+    // int i = 0;
+//\0 is the null terminator used to indicate the end of a string
+    // while (arr[i] != '\0') {
+    //     printf("%c", arr[i]);
+    //     i++;
+    // }    not recommended due to the use of magic numbers
+    
+    char arr[] = "Priyesh"; // string literal
+    // string literal is an array of characters with a null terminator at the end
+
+
+    return 0;
+}
